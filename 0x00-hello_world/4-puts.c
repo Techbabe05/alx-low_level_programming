2e82ea990529d3d72caf105215cdac9dcc8e358c
Techbabe05 <betty.onils@gmail.com>
2022-09-08 12:27:35 -0700

    prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 /*c is fun betty style*/
-BETTY_STYLE="betty-style"
-BETTY_DOC="betty-doc"
 int main(void) { puts("Programming is like building a multilingual puzzle\n"
 		); return (0); }
