636ac2eff9db2186b0ab68f694c419a9d783b9b9
Techbabe05 <betty.onils@gmail.com>
2022-09-15 10:55:06 -0700

    betty

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avioded! \\o/\n");
 
-	/*while (i < 10)*/
-	/*{*/
-	/*putchar(i);*/
-	/*}*/
-
-	printf("Infinite loop avioded! \\o/\n");
-
-	return (0);
+return (0);
 }
