fc8b51d1eefa6b9672c46c9bd02033d926435cc6
Techbabe05 <betty.onils@gmail.com>
2022-09-15 10:09:21 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 include "main.h"
 
 /**
- * tests function that prints is positive or negative
+ * main - tests function that prints is positive or negative
  *retrun; 0
  */
 
-int main (void);
+int main(void)
 {
 	int i;
 
 	i = 0;
-	positive_or _negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
