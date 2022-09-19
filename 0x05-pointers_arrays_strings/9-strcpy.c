ceac02ed90a06804d12a8517f1951b60f430bab2
Techbabe05 <betty.onils@gmail.com>
2022-09-19 10:46:22 -0700

    string that copies

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- *_strcpy - Copy paste string
- *@dest: destination
- *@src: source
- *Return: dest
- */
+*_strcpy - Copy paste string
+*@dest: destination
+*@src: source
+*Return: dest
+*/
 char *_strcpy(char *dest, char *src)
 {
-int inc = 0;
-while (*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
 int inc = 0;
 while (*(src + inc) != '\0')
 {
