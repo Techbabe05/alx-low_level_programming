bd2e0d6678f6681736a6e978d7e06f9e4b9e7fae
Techbabe05 <betty.onils@gmail.com>
2022-09-22 09:26:08 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
-#ifndef main.h
-#define main.h
-
+#ifndef MAIN.H
+#define MAIN.H
+#include <stdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+ */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -13,4 +18,6 @@ char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+void print_buffer(char *b, int size)i;
+
+#endif /*MAIN.H*/
