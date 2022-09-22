41617e1075abdde1e438c1d76338a5761043a6ce
Techbabe05 <betty.onils@gmail.com>
2022-09-22 10:22:53 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int length, j;
-	lenght = 0;
-	while (dest[length] != '\0')
-	{
-		length++;
-	}
-	for (j = 0; j < n && src[j] != '\0'; j++, length++)
-	{
-		dest[length] = src[j];
-	}
-	dest[length] = '\0';
-	return (dest);
-}	
+int length, j;
+lenght = 0;
+while (dest[length] != '\0')
+{
+length++;
+}
+for (j = 0; j < n && src[j] != '\0'; j++, length++)
+{
+dest[length] = src[j];
+}
+dest[length] = '\0';
+
+return (dest);
+}
