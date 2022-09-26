731a14a770ea3f321dc00370ee07dd60e537e4b9
Techbabe05 <betty.onils@gmail.com>
2022-09-26 10:18:35 -0700

    task one

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  *  @dest: destination of copy
  *  @src: memory area to copy
  *  @n: number of bytes to copy
+ *
  *  Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
