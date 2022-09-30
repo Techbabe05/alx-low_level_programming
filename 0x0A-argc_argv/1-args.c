d99e20cb8faaeb3d9813f63be43261a2d61c7c6c
Techbabe05 <betty.onils@gmail.com>
2022-09-30 09:29:33 -0700

    task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  *
  * Return: always 0
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
+
 	return (0);
 }
