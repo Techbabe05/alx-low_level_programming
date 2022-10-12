72a7f272e457681c61250914c750db2c0d699200
Techbabe05 <betty.onils@gmail.com>
2022-10-12 11:26:58 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, r;
+int index = 0;
 
-	if (size > 0 && array && cmp)
-	{
-		for (i = 0; i < size; i++)
-		{
-			r = cmp(array[i]);
-			if (r)
-				break;
-		}
-		if (i < size)
+if (array == NULL || cmp == NULL)
+return (-1);
+
+for (; index < size; index++)
+{
+if (cmp(array[index]) != 0)
+return (index);
+}
+
+return (-1);
+
+}
