9e94872d8482886b4cb073521882f2006b97ae65
Techbabe05 <betty.onils@gmail.com>
2022-10-13 09:49:40 -0700

    task thats sums up

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,6 +9,7 @@ char *symbol;
 void (*print)(va_list arg);
 } printer_t;
 
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -18,4 +19,4 @@ void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /*_VARIADIC_FUNCTIONS_H_ */
