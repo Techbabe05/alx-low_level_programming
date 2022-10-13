1abb98c006e05f2939088b1677afc17d027760e0
Techbabe05 <betty.onils@gmail.com>
2022-10-13 09:40:21 -0700

    function that sums up

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,28 +3,19 @@
 
 /**
  * sum_them_all - sums all its parameters
- * @n: first parameter
+ * @n: number first parameter
  *
- * Return: 0 if n is null
- * or sum of parameters in other case
+ * Return: sum of all parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-
-va_list li;
 int sum = 0;
 unsigned int i;
+va_list valist;
 
-va_start(li, n);
-
-if (n != 0)
-{
+va_start(valist, n);
 for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
-
-va_end(li);
+sum += va_arg(valist, int);
+va_end(valist);
 return (sum);
-
 }
