cb0183dab57da256a82cb54840edf8e44cf9ec34
Techbabe05 <betty.onils@gmail.com>
2022-11-02 22:34:51 -0700

    file descripto

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,7 +28,7 @@ char *create_buffer(char *file)
 }
 
 /**
- * lose_file - Closes file descriptors.
+ *close_file - Closes file descriptors.
  * @fd: The file descriptor to be closed.
  */
 void close_file(int fd)
